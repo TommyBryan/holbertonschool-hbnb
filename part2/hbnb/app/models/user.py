ebbaa12d4df698c9a87171fa9c5f5d157bf4bb86
TommyBryan <tommy0713@gmail.com>
2025-06-21 12:36:47 -0400

    Fixed user class

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,18 +1,58 @@
 from base_class import BaseModel
+import re
 
 class User(BaseModel):
-    def __init__(self, id: str, first_name: str, last_name: str, email: str, is_admin, created_at, updated_at):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.is_admin = is_admin = False
+        self.is_admin = is_admin
 
-        def validate_firts_name(self, first_name: str):
-            if not isinstance(first_name, str):
-                raise TypeError("Firts name mustb be a string")
-            if first_name >= 50:
-                raise ValueError("Maximum length is 50 characters")
-            
-        def validate_last_name(self, last_name: str):
-            if not isinstance(last_name, str):
-                raise TypeError("Last name must be a string")
+    @property
+    def first_name(self):
+        """
+        Get the first name of the user.
+        
+        Returns:
+            str: First name of the user.
+        """
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        if not isinstance(first_name, str):
+            raise TypeError("First name must be a string")
+        if len(first_name) > 50:
+            raise ValueError("Maximum length for first name is 50 characters")
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self, last_name):
+        if not isinstance(last_name, str):
+            raise TypeError("Last name must be a string")
+        if len(last_name) > 50:
+            raise ValueError("Maximum length for last name is 50 characters")
+        self._last_name = last_name
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email):
+        if not isinstance(email, str):
+            raise TypeError("Email must be a string")
+        email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        if not re.match(email_regex, email):
+            raise ValueError("Invalid email format")
+        # Uniqueness check should be handled at a higher level (e.g., database)
+        self._email = email
+
+    @property
+    def is_admin(self):
+        return self._is_admin
