b8e62aef4fe73cf734a855cbdab1e32d7dd41980
TommyBryan <tommybryan0713@gmail.com>
2025-07-01 14:27:49 -0400

    Refactor BaseModel constructor to remove unused parameters

--- part2/hbnb/app/models/base_class.py
+++ part2/hbnb/app/models/base_class.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
