4cefd135e5bd217c8281e989c8f23a74955cac57
TommyBryan <tommybryan0713@gmail.com>
2025-07-03 15:11:11 -0400

    Made changes in facade

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -26,13 +26,13 @@ class HBnBFacade:
         return amenity
 
     def get_amenity(self, amenity_id):
-    # Placeholder for logic to retrieve an amenity by ID
-        pass
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-    # Placeholder for logic to retrieve all amenities
-        pass
+        """
+        Retrieve all amenities.
+        """
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-    # Placeholder for logic to update an amenity
-        pass
+        self.ameity_repo.update(amenity_id, amenity_data)
