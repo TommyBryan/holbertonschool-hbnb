281f0b17101d080c8cfd1a59bba5fe0bf7476353
TommyBryan <tommybryan0713@gmail.com>
2025-08-03 09:46:05 -0400

    Refactor API initialization and namespace imports in app module

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,7 +2,15 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
 from config import DevelopmentConfig
+
+# Import namespaces
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -16,7 +24,18 @@ def create_app(config_class=DevelopmentConfig):
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    from app.api import create_api
-    app.register_blueprint(create_api())
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        doc='/api/v1/'
+    )
+
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
